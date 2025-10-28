3931f415561d2ecab61cefcc649e5fa026d2fc88
Josephina Krause <josephinakrause@MacBook-Air-von-Josephina.local>
2025-10-28 15:48:28 +0100

    Hausaufgeben2

---
+++ src/h3/H3_main.java
@@ -0,0 +1,27 @@
+package h3;
+
+public class H3_main {
+	public static void main(String[] args) { 
+	
+	int i = 101;
+	int j = 8;
+	int k = -10;
+	
+	if (i > j && i < 199 && j < 99){
+		k = 1;
+	}	
+	if (i > j && i > 200 && j < 99){
+		k = 2;
+	}
+	if (i > j && i > 200 && j > 100) {
+		k = 3;
+	}
+	if (i < j && i < 199 && j < 99) {
+		k = 4;
+	}
+	System.out.println (k);
+
+	}
+	
+	}
+
